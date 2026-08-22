2ae7fb4872686a39a9a566b42f9c8c0474b57f2b
CacheCrusher <153100424+suhasgt1@users.noreply.github.com>
2026-08-23 00:16:39 +0530

    Add Top-K-Frequent-Elements (heap-priority-queue)

---
+++ leetcode/heap-priority-queue/347-Top-K-Frequent-Elements.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> count;
+        for (int x : nums)
+            count[x]++;
+        vector<vector<int>> bucket(nums.size() + 1);
+        for (auto [x, f] : count)
+            bucket[f].push_back(x);
+        vector<int> ans;
+        for (int i = nums.size(); i >= 1 && ans.size() < k; i--) {
+            for (int x : bucket[i])
+                ans.push_back(x);
+            if (ans.size() == k)
+                break;
+        }
+        return ans;
+    }
+};
