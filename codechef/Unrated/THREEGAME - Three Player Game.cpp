197c0c26fd14eb1f8cb04dc9b78f4a0e6b6be784
CacheCrusher <153100424+suhasgt1@users.noreply.github.com>
2026-09-02 21:44:32 +0530

    Add Three Player Game (Unrated)

---
+++ codechef/Unrated/THREEGAME - Three Player Game.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t; cin >> t; 
+	while (t--){
+	    long long n; 
+	    cin >> n; 
+	    cout << 3 * (n / 2) + 1 << endl;
+	}
+}
