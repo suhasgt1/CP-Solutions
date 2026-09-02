89b3b7dbe8a64053a63b497969bf869b23a4d074
CacheCrusher <153100424+suhasgt1@users.noreply.github.com>
2026-09-02 20:49:53 +0530

    Add Reach Weight (Unrated)

---
+++ codechef/Unrated/REACHWT - Reach Weight.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t ; 
+	cin >> t ; 
+	while (t--){
+	    int n ; 
+	    cin >> n ; 
+	    cout << n/2*30+ n%2*20 << endl ;
+	}
+
+}
