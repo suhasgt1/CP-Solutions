ca1f75f05dc9bd0c22b01c3ef828cf670ef7ac9d
CacheCrusher <153100424+suhasgt1@users.noreply.github.com>
2026-09-05 01:06:06 +0530

    Add Weighted-Word-Mapping (array)

---
+++ leetcode/array/3838-Weighted-Word-Mapping.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string ans;
+        for (string word : words) {
+            int sum = 0;
+            for (char c : word) sum += weights[c - 'a'];
+            ans += 'z' - sum % 26;
+        }
+        return ans;
+    }
+};
