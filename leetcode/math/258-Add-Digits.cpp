367d47a21cf73801db0700ae7f890bf5775cfef4
CacheCrusher <153100424+suhasgt1@users.noreply.github.com>
2026-09-08 18:33:11 +0530

    Add Add-Digits (math)

--- leetcode/math/258-Add-Digits.cpp
+++ leetcode/math/258-Add-Digits.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int addDigits(int num) {
-        while (num>=10){
-            int sum =0;
-            string s= to_string(num);
-            for(char x : s) sum+=(x-'0');
-            num=sum;
-        }
-        return num;
+        // last submission of mine was efficient but not the most optimal 
+        //there is a formula that gives us sum of all digits
+        // its defined by 1+ (num-1)%9
+        //basically for 17 that is 1+ 16%9 = 1+7 =8
+        // or for say 37 that is 1+(37-1)%9 =1
+        // exception being when num is 0
+        if (num==0) return 0;
+        return (1+(num-1)%9);
     }
 };
