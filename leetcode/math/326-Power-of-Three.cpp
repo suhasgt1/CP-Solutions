2e51db0536b0bbfb02ff4025b90348f7b72a7c80
CacheCrusher <153100424+suhasgt1@users.noreply.github.com>
2026-09-12 00:01:30 +0530

    Add Power-of-Three (math)

--- leetcode/math/326-Power-of-Three.cpp
+++ leetcode/math/326-Power-of-Three.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if (n<1)return false;  
-        while (n!=1){
-            if (n%3!=0) return false;
-            n/=3;
-        }
-        return true;
+        if (n<1) return false; 
+        while (n%3==0) n/=3;
+        if (n==1) return true;
+        return false;
     }
 };
