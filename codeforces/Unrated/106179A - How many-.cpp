3a41a40e599da9aaee93503c4a7d67c6c0479b75
CacheCrusher <153100424+suhasgt1@users.noreply.github.com>
2026-09-12 23:34:38 +0530

    Add How many- (Unrated)

---
+++ codeforces/Unrated/106179A - How many-.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define l long
+#define inf 1e18
+#define vi vector<int>
+#define vll vector<ll>
+#define vs vector<string>
+#define pi pair<int,int>
+#define vp vector<pair<int,int>>
+#define vit vector<int>::iterator
+#define mii map<int,int>
+#define umii unordered_map<int,int>
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n ; 
+    cin>> n ; 
+    cout << n *3 << endl; 
+    
+
+    return 0;
+}
