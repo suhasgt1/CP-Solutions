cae0d89617b3e04cb7db9d5ba3b5ffd731366b39
CacheCrusher <153100424+suhasgt1@users.noreply.github.com>
2026-09-16 17:25:59 +0530

    Add Unit Array (800)

--- codeforces/800/1834A - Unit Array.cpp
+++ codeforces/800/1834A - Unit Array.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 
 #define ll long long
+#define l long
+#define inf 1e18
+#define vi vector<int>
+#define vll vector<ll>
+#define vs vector<string>
+#define pi pair<int,int>
+#define vp vector<pair<int,int>>
+#define vit vector<int>::iterator
+#define mii map<int,int>
+#define umii unordered_map<int,int>
 
 int main() {
     ios::sync_with_stdio(false);
@@ -13,20 +23,19 @@ int main() {
     while (t--) {
         ll n;
         cin >> n;
-
-        int countn = 0, x;
-
+        int pos=0 , neg=0, ans=0;
+        vector<ll> arr(n);
         for (int i = 0; i < n; i++) {
-            cin >> x;
-            if (x == -1) countn++;
+            cin >> arr[i];
+            (arr[i]==-1) ? neg++: pos++;
         }
-
-        int ans = max(0, countn - (int)n / 2);
-
-        if ((countn - ans) % 2)
-            ans++;
-
-        cout << ans << endl;
+        while (pos < neg || neg % 2) {
+        pos++;
+        neg--;
+        ans++;
+        }
+        cout << ans << endl; 
+        
     }
 
     return 0;
